f8870b1297a81513b2b1355b4722abfa61569ba4
Bohan Yang <youthdna@live.com>
2019-10-18 23:58:01 +0900

    Object cache expiration

--- src/Controller/GalleryController.php
+++ src/Controller/GalleryController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\RepositoryContract;
+use DateTimeImmutable;
+use DateTimeZone;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Cache\CacheInterface;
@@ -10,8 +12,12 @@ use Symfony\Contracts\Cache\ItemInterface;
 
 class GalleryController extends AbstractController
 {
+
     private $repository;
+    
     private $cache;
+
+//    private const TZ = 'Asia/Shanghai';
 
     public function __construct(RepositoryContract $repository, CacheInterface $cache)
     {
@@ -25,7 +31,15 @@ class GalleryController extends AbstractController
     public function image(string $name)
     {
         $result = $this->cache->get("image.$name", function (ItemInterface $item) use ($name) {
-            $item->expiresAfter(600);
+//            $tz = new DateTimeZone(self::TZ);
+//            $now = new DateTimeImmutable(null, $tz);
+//            $expiresAt = new DateTimeImmutable('15:04', $tz);
+//
+//            if ($expiresAt < $now) {
+//                $expiresAt = new DateTimeImmutable('tomorrow 15:04', $tz);
+//            }
+//
+//            $item->expiresAt($expiresAt);
 
             return $this->repository->getImage($name);
         });
