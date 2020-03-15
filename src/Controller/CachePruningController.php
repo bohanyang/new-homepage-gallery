b4c3ccd44583fe8b8cbbd802e28be89e6777cbf7
Bohan Yang <youthdna@live.com>
2020-03-15 21:39:22 +0900

    Add cache clear

--- src/Controller/CachePruningController.php
+++ src/Controller/CachePruningController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use OTPHP\TOTPInterface;
+use Psr\Cache\CacheItemPoolInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Cache\PruneableInterface;
@@ -28,8 +29,24 @@ final class CachePruningController extends AbstractController
 
     public function index(string $token)
     {
-        if ($this->cache instanceof PruneableInterface && $this->TOTP->verify($token) && $this->cache->prune()) {
+        if (
+            $this->cache instanceof PruneableInterface &&
+            $this->TOTP->verify($token) &&
+            $this->cache->prune()
+        ) {
             $this->logger->notice('Cache pruned');
+        }
+
+        return $this->redirectToRoute('homepage');
+    }
+
+    public function clear(string $token)
+    {
+        if ($this->cache instanceof CacheItemPoolInterface &&
+            $this->TOTP->verify($token) &&
+            $this->cache->clear()
+        ) {
+            $this->logger->notice('Cache cleared');
         }
 
         return $this->redirectToRoute('homepage');
