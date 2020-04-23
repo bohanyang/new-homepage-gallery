b576994fd5aa483e95cb9182614e7d3e467e7e20
Bohan Yang <youthdna@live.com>
2020-04-23 23:08:19 +0900

    Rename oracle subs

--- src/EventSubscriber/OracleSessionInitSubscriber.php
+++ src/EventSubscriber/OracleSessionInitSubscriber.php
@@ -3,8 +3,9 @@
 namespace App\EventSubscriber;
 
 use Doctrine\DBAL\Event\ConnectionEventArgs;
+use Doctrine\DBAL\Event\Listeners\OracleSessionInit;
 
-class OracleSessionInit extends \Doctrine\DBAL\Event\Listeners\OracleSessionInit
+class OracleSessionInitSubscriber extends OracleSessionInit
 {
     public function postConnect(ConnectionEventArgs $args)
     {
